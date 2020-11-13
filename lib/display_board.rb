03b3c568d1ad1d5ad4baf347325b964a881ecb63
Tenny Agustin <lauren.marie.agustin@gmail.com>
2020-11-13 18:33:00 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  board = ["   ", "|", "   ", "\n", "   ", "|", "   ", "\n", "   ", "|", "   ", "\n"]
-  print board
+  print ["   ", "|", "   ", "|", "   "]
+  puts ["   ", "|", "   ", "|", "   "]
+  puts ["   ", "|", "   ", "|", "   "]
 end
 
 display_board
